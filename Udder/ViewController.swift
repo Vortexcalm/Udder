590548bfad0eeeb7f075a9f14706827afe01c54d
Tristan Hughes <hughes_tek@hotmail.com>
2020-05-26 19:21:39 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tristan Hughes on 5/26/20.
+//  Copyright © 2020 Tristan Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
